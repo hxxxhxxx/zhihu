fe7911ab71feb7a40ba815d6a65b942e1b3b077c
hxxxhxxx <hxxxhxxx@protonmial.com>
2017-03-15 10:57:19 +0800

    init

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.admin')
+
+@section('content')
+    <p>hello world</p>
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -48,6 +48,7 @@
 
                     <!-- Right Side Of Navbar -->
                     <ul class="nav navbar-nav navbar-right">
+                        <li><a href="{{ url('admin') }}" target="_blank">后台管理</a></li>
                         <!-- Authentication Links -->
                         @if (Auth::guest())
                             <li><a href="{{ route('login') }}">登录</a></li>
